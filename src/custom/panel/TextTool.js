95258b5a8cd85e550455e4ff723298a404d100d4
Radovan Kuka <radovan_kuka@datalan.sk>
2014-10-08 16:22:45 +0200

    add text tool for Panel

---
+++ src/custom/panel/TextTool.js
@@ -0,0 +1,20 @@
+/**
+ * Created by rkuka on 7. 10. 2014.
+ */
+Ext.define('Dext.custom.panel.TextTool', {
+    extend: 'Ext.panel.Tool',
+    alias: 'widget.texttool',
+
+    type: 'text',
+
+    renderTpl: ['<span class="{baseCls}-{type} {specificCls}" role="presentation" >{caption}</span>'],
+
+    initComponent: function(){
+        Ext.applyIf(this.renderData, {
+            caption: this.caption,
+            specificCls: this.linkCls
+        });
+
+        this.callParent();
+    }
+});
