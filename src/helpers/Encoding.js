493fa4f3f3faf1e7af1266ab9a2110d61124768f
Radovan Kuka <radovan_kuka@datalan.sk>
2014-12-09 16:32:58 +0100

    add html encoding helper method

--- src/helpers/Encoding.js
+++ src/helpers/Encoding.js
@@ -1,11 +1,18 @@
 Ext.define('Dext.helpers.Encoding', {
     singleton: true,
 
-    object: typeof exports !== 'undefined' ? exports : window,
     chars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
 
+    encodeHTMLmap: {
+        '&': '&amp;',
+        '\'': '&#39;',
+        '"': '&quot;',
+        '<': '&lt;',
+        '>': '&gt;'
+    },
+
     /**
-     * Btoa implementation.
+     * Btoa polyfill.
      *
      * Source: [https://gist.github.com/999166] by [https://github.com/nignag]
      *
@@ -13,17 +20,17 @@ Ext.define('Dext.helpers.Encoding', {
      * @return {String} encoded string
      */
     btoa: function(input){
-        if(this.object.btoa){
-            return this.object.btoa(input);
+        if(window.btoa){
+            return window.btoa(input);
         }
 
         var str = String(input);
         for(// initialize result and counter
-        var block, charCode, idx = 0, map = this.chars, output = ''; // if the next str index does not exist:
-        //   change the mapping table to "="
-        //   check if d has no fractional digits
-        str.charAt(idx | 0) || (map = '=', idx % 1); // "8 - idx % 1 * 8" generates the sequence 2, 4, 6, 8
-        output += map.charAt(63 & block >> 8 - idx % 1 * 8)){
+            var block, charCode, idx = 0, map = this.chars, output = ''; // if the next str index does not exist:
+            //   change the mapping table to "="
+            //   check if d has no fractional digits
+            str.charAt(idx | 0) || (map = '=', idx % 1); // "8 - idx % 1 * 8" generates the sequence 2, 4, 6, 8
+            output += map.charAt(63 & block >> 8 - idx % 1 * 8)){
             charCode = str.charCodeAt(idx += 3 / 4);
             if(charCode > 0xFF){
                 throw new Error('\'btoa\' failed: The string to be encoded contains characters outside of the Latin1 range.');
@@ -34,7 +41,7 @@ Ext.define('Dext.helpers.Encoding', {
     },
 
     /**
-     * Atob implementation.
+     * Atob polyfill.
      *
      * Source: [https://gist.github.com/999166] by [https://github.com/nignag]
      *
@@ -42,8 +49,8 @@ Ext.define('Dext.helpers.Encoding', {
      * @return {String} decoded string
      */
     atob: function(input){
-        if(this.object.atob){
-            return this.object.atob(input);
+        if(window.atob){
+            return window.atob(input);
         }
 
         var str = String(input).replace(/=+$/, '');
@@ -51,14 +58,20 @@ Ext.define('Dext.helpers.Encoding', {
             throw new Error('\'atob\' failed: The string to be decoded is not correctly encoded.');
         }
         for(// initialize result and counters
-        var bc = 0, bs, buffer, idx = 0, output = ''; // get next character
-        buffer = str.charAt(idx++); // character found in table? initialize bit storage and add its ascii value;
-        ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer, // and if not first of each 4 characters,
-            // convert the first 8 bits to one ascii character
-        bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0){
+            var bc = 0, bs, buffer, idx = 0, output = ''; // get next character
+            buffer = str.charAt(idx++); // character found in table? initialize bit storage and add its ascii value;
+            ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer, // and if not first of each 4 characters,
+                // convert the first 8 bits to one ascii character
+                bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0){
             // try to find character in table (0-63, not found => -1)
             buffer = this.chars.indexOf(buffer);
         }
         return output;
+    },
+
+    html: function(text){
+        return text.replace(/[&"'<>]/g, Ext.bind(function(match){
+            return this.encodeHTMLmap[match];
+        }, this));
     }
 });
