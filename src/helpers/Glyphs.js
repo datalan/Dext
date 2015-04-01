c2c7c021a0c55766e613d504e25126ab03df2bca
Peter Ivan <peter.ivan@proxia.org>
2015-04-01 17:04:16 +0200

    [TASK] Added glyph "empty".

--- src/helpers/Glyphs.js
+++ src/helpers/Glyphs.js
@@ -36,7 +36,8 @@ Ext.define('Dext.helpers.Glyphs', {
         save: 0xf0c7,
         gear: 0xf013,
         question: 0xf128,
-        detail: 0xf05a
+        detail: 0xf05a,
+        empty: 0xf016
     },
 
     /**
