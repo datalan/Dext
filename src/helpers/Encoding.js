984fc0fbc6d3c88b83c794a4e5f60f9c406dfdc2
Milan Šafárik <milan.safarik@itkonzult.sk>
2015-04-28 13:28:21 +0200

    minor change

--- src/helpers/Encoding.js
+++ src/helpers/Encoding.js
@@ -45,24 +45,8 @@ Ext.define('Dext.helpers.Encoding', {
      * @return {String} encoded string
      */
     encodedBtoa: function(input){
-        if(window.btoa){
-            return window.btoa(this.utf8Encode(input));
-        }
-
-        var str = this.utf8Encode(String(input));
-        for(// initialize result and counter
-            var block, charCode, idx = 0, map = this.chars, output = ''; // if the next str index does not exist:
-            //   change the mapping table to "="
-            //   check if d has no fractional digits
-            str.charAt(idx | 0) || (map = '=', idx % 1); // "8 - idx % 1 * 8" generates the sequence 2, 4, 6, 8
-            output += map.charAt(63 & block >> 8 - idx % 1 * 8)){
-            charCode = str.charCodeAt(idx += 3 / 4);
-            if(charCode > 0xFF){
-                throw new Error('\'btoa\' failed: The string to be encoded contains characters outside of the Latin1 range.');
-            }
-            block = block << 8 | charCode;
-        }
-        return output;
+        var encodedInput = this.utf8Encode(input);
+        return this.btoa(encodedInput);
     },
 
     /**
